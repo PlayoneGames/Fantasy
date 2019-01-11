7f7a899d4865132104e3ec0c12bb3e5b951e1c88
Gopal Bhatnagar <gopal.bhatnagar@playone.com>
2019-01-11 11:26:21 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,31 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ValidatorsModule } from '../validators/validators.module';
 
 import { MyApp } from './app.component';
+
+import { FormPage } from '../pages/form/form';
+import { UserPage } from '../pages/user/user';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FormPage,
+    UserPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ValidatorsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    FormPage,
+    UserPage,
     HomePage
   ],
   providers: [
